c71c22d20366190c13a56b3e4c7daca782f5f473
Michael Fleet <mf@f1337.us>
2012-05-24 16:21:08 -0400

    Stubbing out OMLengthValidator.

---
+++ ActiveModel/validators/LengthValidator/OMActiveModel+LengthValidation.m
@@ -0,0 +1,13 @@
+//
+//  OMActiveModel+LengthValidation.m
+//  ActiveModel
+//
+//  Created by Michael Fleet on 5/24/12.
+//  Copyright (c) 2012 Michael R. Fleet. All rights reserved.
+//
+
+#import "OMActiveModel+LengthValidation.h"
+
+@implementation OMActiveModel (LengthValidation)
+
+@end
